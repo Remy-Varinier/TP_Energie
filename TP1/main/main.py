ee7cfc6be49a1dd187054250584bed84e3ea4e9c
Aurelien-Bourret <bourreta@etu.univ-grenoble-alpes.fr>
2022-04-08 10:59:35 +0200

    Implémentation buildTour (optimal sur les distances)
    Renommage fonctions findNearVisit -> findNearestVisit
    Ajout findNearestDepot() (inutile pour l'instant)
    Refactorisation variables sur buildTours() et buildTour()

--- TP1/main/main.py
+++ TP1/main/main.py
@@ -21,7 +21,7 @@ with open(folder+'visits.csv', newline='') as csvfile:
 config = ConfigParser()
 config.read(folder+"vehicle.ini")
 
-distance = np.loadtxt(folder+"distances.txt")
+distances = np.loadtxt(folder + "distances.txt")
 times = np.loadtxt(folder+"times.txt")
 
 
@@ -33,6 +33,89 @@ def getStrFromIni(value: str):
     return config.get("Vehicle", value)
 
 
+def buildTours(allVisits: typing.List[Visit], vehicle: Vehicle, distance) -> typing.List[str]:
+    depot = allVisits.pop(0)
+    v = vehicle.clone()
+    (the_visits, the_tour) = buildTour(allVisits, v, depot, distance)
+    result = [the_tour]
+    while len(the_visits) > 0:
+        v = vehicle.clone()
+        (the_visits, the_tour) = buildTour(the_visits, v, depot, distance)
+        result.append(the_tour)
+    return result
+
+
+def buildTour(listVisit: typing.List[Visit], vehicle: Vehicle, depot: Visit, distance) -> typing.Tuple[typing.List[Visit], str]:
+    currentVisit = depot
+    notFull = True
+    tourRes = str(currentVisit.visitId)
+    vehicle.setCapacity(vehicle.capacity)
+    while notFull:
+        try:
+            futurVisit = findNearestVisit(listVisit, currentVisit, distance)
+            ### Pour plus tard : nearestDepot = findNearestDepot(listVisit, actualVisit, distances)
+        except IndexError:
+            break #La liste de visites restantes est vide, on a fini
+
+        distMin = distance[currentVisit.visitId][futurVisit.visitId]
+        time = times[currentVisit.visitId][futurVisit.visitId]
+
+        if not(vehicle.canAddTime(time + times[futurVisit.visitId][depot.visitId])):
+            #La journée du véhicule est finie
+            notFull = False
+        elif not(vehicle.canAddKilometer(distMin + distance[futurVisit.visitId][depot.visitId])):
+            #Le véhicule ne peut pas effectuer la distance puis retourner au dépôt, il faut le recharger
+            vehicle.addKilometer(distance[currentVisit.visitId][depot.visitId])
+            vehicle.addTime(times[currentVisit.visitId][depot.visitId])
+            tourRes += ",R"
+            vehicle.recharge() #charge FAST
+            currentVisit = depot
+        elif not(vehicle.canRemoveCapacity(futurVisit.demand)):
+            #La destination a une demande trop forte, il faut réapprovisionner le véhicule en allant au dépôt
+            vehicle.addKilometer(distance[currentVisit.visitId][depot.visitId])
+            vehicle.addTime(times[currentVisit.visitId][depot.visitId])
+            tourRes += ",C"
+            vehicle.setCapacity(vehicle.capacity)
+            currentVisit = depot
+        else:
+            #On peut effectuer la livraison
+            vehicle.addKilometer(distMin)
+            vehicle.addTime(time)
+            vehicle.removeCapacity(futurVisit.demand)
+            tourRes += ","+str(futurVisit.visitId)
+            currentVisit = futurVisit
+            listVisit.remove(futurVisit)
+            if len(listVisit) == 0:
+                notFull = False
+
+    return (listVisit, tourRes)
+
+
+def findNearestVisit(listVisit: typing.List[Visit], fromVisit: Visit, distance) -> Visit:
+    distMin = distance[fromVisit.visitId][listVisit[0].visitId] #raises IndexError if listVisit is empty!
+    futurVisit = listVisit[0]
+    for j in range(0, len(listVisit)-1):
+        tempDist = distance[fromVisit.visitId][listVisit[j].visitId]
+        if distMin > tempDist:
+            futurVisit = listVisit[j]
+            distMin = tempDist
+    return futurVisit
+
+def findNearestDepot(listVisit: typing.List[Visit], fromVisit: Visit, distance) -> Visit:
+    distMin = distance[fromVisit.visitId][listVisit[0].visitId]  #raises IndexError if listVisit is empty!
+    listDepots = list(filter(lambda x: x.visitName == "Depot", listVisit))
+    futurVisit = None
+    for j in range(0, len(listDepots) - 1):
+        tempDist = distance[fromVisit.visitId][listDepots[j].visitId]
+        if distMin > tempDist:
+            futurVisit = listDepots[j]
+            distMin = tempDist
+    return futurVisit
+
+
+
+
+### TP1 CONSTRUIRE LES TOURS ###
 vroom = Vehicle(
     600,
     getIntFromIni("capacity"),
@@ -42,66 +125,9 @@ vroom = Vehicle(
     getStrFromIni("start_time"),
     getStrFromIni("end_time")
 )
-tourVisits = []
-tourVisits.append(listVisits[0])
-tourVisits.append(listVisits[1])
-tourVisits.append(listVisits[0])
-tourVisits.append(listVisits[1])
-#tour = Tour(tourVisits, vroom)
-# print(tour.calcKilometre(distance))
-
-
-def buildTours(allVisits: list[Visit], vehicle: Vehicle, distance) -> list[Tour]:
-    depot = allVisits.pop(0)
-    v = vehicle.clone()
-    t = buildTour(allVisits, v, depot, distance)
-    result = []
-    result.append(str(t[0]))
-    result = []
-    while len(t[0]) > 0:
-        v = vehicle.clone()
-        t: tuple = buildTour(t[0], v, depot, distance)
-        result.append(str(t[1]))
-    return result
-
-
-def buildTour(listVist: typing.List[Visit], vehicle: Vehicle, depot: Visit) -> typing.Tuple[typing.List[Visit], Tour]:
-    actualVisit = depot
-    notFull = True
-    visits = []
-    visits.append(actualVisit)
-    while notFull == True:
-        visits = []
-        futurVisit: Visit = findNearVisit(listVist, actualVisit, distance)
-        distMin = distance[actualVisit.visitId][futurVisit.visitId]
-
-        time = times[actualVisit.visitId][futurVisit.visitId]
-        if vehicle.canAddKilometer(distMin + distance[futurVisit.visitId][depot.visitId]) == False & vehicle.canAddTime(time + times[futurVisit.visitId][depot.visitId]) == False:
-            notFull = False
-        else:
-            vehicle.addKilometer(distMin)
-            vehicle.addTime(time)
-            visits.append(futurVisit)
-            vehicle.removeCharge(futurVisit.demand)
-            listVisits.remove(futurVisit)
-            if len(listVisits) == 0:
-                notFull = False
-    return (listVisits, Tour(visits, vehicle))
-
-
-def findNearVisit(listVisit: list[Visit], visit: Visit, distance) -> Visit:
-    distMin = distance[visit.visitId][listVisit[0].visitId]
-    tempDist = -1
-    futurVisit = listVisit[0]
-    for j in range(0, len(listVisit)-1):
-        tempDist = distance[visit.visitId][listVisit[j].visitId]
-        if distMin > tempDist:
-            futurVisit = listVisit[j]
-            distMin = tempDist
-    return futurVisit
-
-
-print(buildTours(listVisits, vroom, distance))
+the_result = buildTours(listVisits, vroom, distances)
+for r in the_result:
+    print(r)
 
 
 def findVoisinage1(listTours: typing.List[Tour], tIndex: int, v1Index: int, v2Index: int):
@@ -162,20 +188,3 @@ def findVoisinage4(listTours: typing.List[Tour], tIndex: int, crIndex: int=0, sh
     except IndexError:
         "Voisinage impossible"
 
-
-vroom = Vehicle(
-    6,
-    getIntFromIni("capacity"),
-    getIntFromIni("charge_fast"),
-    getIntFromIni("charge_medium"),
-    getIntFromIni("charge_slow"),
-    getStrFromIni("start_time"),
-    getStrFromIni("end_time")
-)
-tourVisits = []
-tourVisits.append(listVisits[0])
-tourVisits.append(listVisits[1])
-tourVisits.append(listVisits[0])
-tourVisits.append(listVisits[1])
-tour = Tour(tourVisits, vroom)
-print(tour.calcKilometre(distance))
